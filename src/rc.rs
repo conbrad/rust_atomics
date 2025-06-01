c592766d808db390e0d3b779e6b872327520a22f
Conor Brady <con.brad@gmail.com>
2025-05-31 17:06:48 -0700

    not threadsafe

--- src/rc.rs
+++ src/rc.rs
@@ -1,10 +1,18 @@
-use std::rc::Rc;
+use std::{rc::Rc, thread};
 
 pub fn same_ref() {
     let a: Rc<[i32; 3]> = Rc::new([1, 2, 3]);
     let b: Rc<[i32; 3]> = a.clone();
 
     assert_eq!(a.as_ptr(), b.as_ptr()); // Same allocation!
+}
+
+pub fn same_ref_not_threadsafe() {
+    let a: Rc<[i32; 3]> = Rc::new([1, 2, 3]);
+    let b: Rc<[i32; 3]> = a.clone();
+
+    thread::spawn(move || dbg!(a));
+    thread::spawn(move || dbg!(b));
 }
 
 #[cfg(test)]
@@ -14,5 +22,10 @@ mod rc_tests {
     #[test]
     fn test_same_ref() {
         same_ref();
+    }
+
+    #[test]
+    fn test_same_ref_not_threadsafe() {
+        same_ref_not_threadsafe();
     }
 }
